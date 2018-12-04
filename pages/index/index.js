8a3352bf104f58cc02c04febf6b38717e6da2e1f
HughmanLum <HughmanLum@gmail.com>
2018-12-04 17:38:08 +0800

    update

---
+++ pages/index/index.js
@@ -0,0 +1,206 @@
+// pages/index/index.js
+// const app = getApp()
+var util1 = require('../../utils/util1.js')
+var app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    feed: [],
+    feed_length: 0
+  },
+  //事件处理函数
+  bindItemTap: function () {
+    wx.navigateTo({
+      url: '../answer/answer'
+    })
+  },
+  bindQueTap: function () {
+    wx.navigateTo({
+      url: '../question/question'
+    })
+  },
+  // bindViewTap: function () {
+  //   wx.navigateTo({
+  //     url: '../logs/logs'
+  //   })
+  // },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    this.getData();
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  upper: function () {
+    wx.showNavigationBarLoading()
+    this.refresh();
+    console.log("upper");
+    setTimeout(function () { wx.hideNavigationBarLoading(); wx.stopPullDownRefresh(); }, 2000);
+  },
+  lower: function (e) {
+    wx.showNavigationBarLoading();
+    var that = this;
+    setTimeout(function () { wx.hideNavigationBarLoading(); that.nextLoad(); }, 1000);
+    console.log("lower")
+  },
+  scroll: function (e) {
+   console.log("scroll")
+  },
+
+  // 网络请求数据, 实现首页刷新
+  refresh0: function () {
+    var index_api = '';
+    util.getData(index_api)
+      .then(function (data) {
+        //this.setData({
+        //
+        //});
+        console.log(data);
+      });
+  },
+
+  //使用本地 fake 数据实现刷新效果
+  getData: function () {
+    var feed = util1.getData2();
+    console.log("loaddata");
+    var feed_data = feed.data;
+    this.setData({
+      feed: feed_data,
+      feed_length: feed_data.length
+    });
+  },
+  refresh: function () {
+    wx.showToast({
+      title: '刷新中',
+      icon: 'loading',
+      duration: 3000
+    });
+    var feed = util1.getData2();
+    console.log("loaddata");
+    var feed_data = feed.data;
+    this.setData({
+      feed: feed_data,
+      feed_length: feed_data.length
+    });
+    setTimeout(function () {
+      wx.showToast({
+        title: '刷新成功',
+        icon: 'success',
+        duration: 2000
+      })
+    }, 3000)
+
+  },
+
+  //使用本地 fake 数据实现继续加载效果
+  nextLoad: function () {
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 4000
+    })
+    var next = util.getNext();
+    console.log("continueload");
+    var next_data = next.data;
+    this.setData({
+      feed: this.data.feed.concat(next_data),
+      feed_length: this.data.feed_length + next_data.length
+    });
+    setTimeout(function () {
+      wx.showToast({
+        title: '加载成功',
+        icon: 'success',
+        duration: 2000
+      })
+    }, 3000)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  // /**
+  //  * 生命周期函数--监听页面显示
+  //  */
+  // onShow: function () {
+
+  // },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
